8427d1787d660c7672a3d92b587ef70f8b6a66d2
kuntuong <86180726+kuntuong@users.noreply.github.com>
2021-11-10 19:32:39 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from pygame import *
+
+# initialize the font class
+font.init()
+
+# initialize the clock class
+clock = time.Clock()
+
+game = True
+while game:
+    # call the keys pressed and FPS times
+    keys_pressed = key.get_pressed()
+    clock.tick(60)
+
+    for e in event.get():
+        # gives the player option to 
+        # quit the game
+        if e.type == QUIT:
+            game = False
+
+    # always update the screen
+    display.update()
