e27b60fcd54d57c44f66aff05fe6811eeab7ecff
kuntuong <86180726+kuntuong@users.noreply.github.com>
2021-11-10 19:51:17 +0700

    background and class

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 from pygame import *
 
+window = display.set_mode((700, 500))
+display.set_caption("Ping Pong")
+
 # initialize the font class
 font.init()
 
 # initialize the clock class
 clock = time.Clock()
+
+background = transform.scale(image.load("background.jpg"), (700, 500))
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, img, x, y, width, height, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(img), (width, height))
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def blit_image(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Ball(GameSprite):
+    def update():
+        pass
+
+class Paddle(GameSprite):
+    def update():
+        pass
 
 game = True
 while game:
